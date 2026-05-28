48c2768ea66c22e5015203b527d0ffa400e475ee
kawshal <apluskidsdev001@gmail.com>
2026-05-28 18:00:54 +0530

    Update responsive taskbar layout

--- aplus_kids_web/src/app/layout.tsx
+++ aplus_kids_web/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className={`${fredoka.className} min-h-full flex flex-col pb-24 md:pb-0`}>
+      <body className={`${fredoka.className} min-h-full flex flex-col pb-24 laptop:pb-0`}>
         <TaskBar />
         {children}
       </body>
